d872de50218de5315bba45a56e93dbb9105cf611
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-04-20 22:53:57 +0900

    대문자 변환 방법 변경

--- BaekJoon_3181 줄임말 만들기/소스.cpp
+++ BaekJoon_3181 줄임말 만들기/소스.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 string no[MAX] = { "i", "pa", "te", "ni", "niti", "a", "ali", "nego", "no", "ili" };
-string appand[] = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+//string appand[] = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
 int main()
 {
 	int i, j = 0;
@@ -15,7 +15,8 @@ int main()
 		for (i = 0; i < MAX && j != 1; i++)
 			if (no[i] == input) break;
 		if (j == 1 || i == MAX)
-			result += appand[input[0] - 'a'];
+			result += input[0] - 32;
+			//result += appand[input[0] - 'a'];
 	}
 	cout << result;
 	return 0;
